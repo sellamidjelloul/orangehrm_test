ede45322f08ce6e1a966031560d378e3914ce618
Djelloul Sellami <sellami.djelloul@gmail.com>
2020-09-15 12:38:03 +0100

    Modification fichiers: loginSuccess.php et noHeader.php

--- symfony/apps/orangehrm/templates/noHeader.php
+++ symfony/apps/orangehrm/templates/noHeader.php
@@ -1,16 +1,17 @@
-<?php require_once '_header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OrangeHRM</title>
+    <link href="<?php echo theme_path('css/bootstrap.min.css')?>" rel="stylesheet" type="text/css"/>
+	<?php echo stylesheet_tag(theme_path('css/login_new.css')); ?>
+    <link rel="shortcut icon" href="<?php echo theme_path('images/favicon.ico')?>" />
+</head>
+<body>
+	<?php echo $sf_content ?>
 
-    </head>
-    <body>
-      
-        <div id="wrapper">
-            
-            <div id="content">
-
-                  <?php echo $sf_content ?>
-
-            </div> <!-- content -->
-          
-        </div> <!-- wrapper -->
-        
-<?php require_once '_footer.php'; ?>
+    <!-- Custom JavaScript files -->
+    <script src="<?php echo theme_path('js/jquery.min.js')?>"></script>
+    <script src="<?php echo theme_path('js/popper.min.js')?>"></script>
+    <script src="<?php echo theme_path('js/bootstrap.min.js')?>"></script>
+</body>
+</html>
